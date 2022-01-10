387f7347cfd7db43a710d2e617181a9748401cd8
CarolynnFleming <cfcatpink@gmail.com>
2022-01-10 15:42:07 -0800

    la

--- create/create.js
+++ create/create.js
@@ -8,15 +8,14 @@ import {
 const form = document.querySelector('.bunny-form');
 const logoutButton = document.getElementById('logout');
 
-form.addEventListener('submit', async e => {
+form.addEventListener('submit', async (e) => {
     // prevent default
     e.preventDefault();
     // get the name and family id from the form
     const data = new FormData(form);
     // use createBunny to create a bunny with this name and family id
-    const name = data.get('name');
-    const family_id = data.get('family_id');
-
+    const name = data.get('bunny-name');
+    const family_id = data.get('family-id');
     await createBunny({
         name: name,
         family_id: family_id
--- fetch-utils.js
+++ fetch-utils.js
@@ -11,7 +11,7 @@ export async function getFamilies() {
     // fetch all families and their bunnies
     const response = await client
         .from('loving_families')
-        .select(`*, fuzzy_bunnies (*)`);
+        .select('*, fuzzy_bunnies (*)');
 
     return checkError(response);    
 }
